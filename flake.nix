99e0367c88ce418e23c09194d1bc8d80cfdcfcbb
Ângelo Nuffer <angelonuffer@gmail.com>
2026-08-15 11:12:07 -0300

    Adiciona configuração inicial do ambiente de desenvolvimento Rust

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A standard Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+
+          # Environment variables
+          RUST_BACKTRACE = 1;
+        };
+      }
+    );
+}
